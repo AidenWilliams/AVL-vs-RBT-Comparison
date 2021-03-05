1c02279f403a85a912c787983ae4eb70df57922f
Aiden <mw3dbest@gmail.com>
2021-03-05 20:30:30 +0100

    Tree inserting works, however output needs some work

--- AVL.py
+++ AVL.py
@@ -1,8 +1,8 @@
 class avl:
     def __init__(self):
         self.value = 0
-        self.left = avl()
-        self.right = avl()
+        self.left = None
+        self.right = None
 
 
 class avl_tree:
@@ -26,28 +26,28 @@ class avl_tree:
 
     @staticmethod
     def rr_rotation(parent: avl):
-        tree = parent.right
-        parent.right = tree.left
-        tree.left = parent
-        return tree
+        child = parent.right
+        parent.right = child.left
+        child.left = parent
+        return child
 
     @staticmethod
     def ll_rotation(parent: avl):
-        tree = parent.left
-        parent.left = tree.right
-        tree.right = parent
-        return tree
+        child = parent.left
+        parent.left = child.right
+        child.right = parent
+        return child
 
     @staticmethod
     def lr_rotation(parent: avl):
-        tree = parent.left
-        parent.left = avl_tree.rr_rotation(tree)
+        child = parent.left
+        parent.left = avl_tree.rr_rotation(child)
         return avl_tree.ll_rotation(parent)
 
     @staticmethod
     def rl_rotation(parent: avl):
-        tree = parent.right
-        parent.right = avl_tree.ll_rotation(tree)
+        child = parent.right
+        parent.right = avl_tree.ll_rotation(child)
         return avl_tree.rr_rotation(parent)
 
     def balance(self, tree: avl):
@@ -64,7 +64,7 @@ class avl_tree:
                 tree = avl_tree.rr_rotation(tree)
         return tree
 
-    def insert(self, tree: avl, value: int):
+    def insert(self, tree, value: int):
         if tree is None:
             tree = avl()
             tree.value = value
@@ -77,31 +77,32 @@ class avl_tree:
             tree.right = self.insert(tree.right, value)
         return self.balance(tree)
 
+    # TODO: Fix show and order functions
     def show(self, tree: avl, depth=1):
         if tree is not None:
             self.show(tree.right, depth + 1)
-            print(" ")
-            if tree is self.right:
-                print("Root -> ")
+            print(" ", end="")
+            if tree is self.root:
+                print("Root -> ", end="")
             for i in range(depth):
-                if tree is self.right:
+                if tree is self.root:
                     break
 
-                print(" " + str(tree.value))
+                print(" " + str(tree.value), end="")
                 self.show(tree.left, depth + 1)
 
     def in_order(self, tree: avl):
         if tree is None:
             return
         self.in_order(tree.left)
-        print(str(tree.value) + " ")
+        print(str(tree.value) + " ", end="")
         self.in_order(tree.right)
 
     def pre_order(self, tree: avl):
         if tree is None:
             return
 
-        print(str(tree.value) + " ")
+        print(str(tree.value) + " ", end="")
         self.in_order(tree.left)
         self.in_order(tree.right)
 
@@ -111,4 +112,28 @@ class avl_tree:
 
         self.in_order(tree.left)
         self.in_order(tree.right)
-        print(str(tree.value) + " ")
+        print(str(tree.value) + " ", end="")
+
+
+def __test__():
+    avlTree = avl_tree()
+    # First insert into None node (create root)
+    r = avlTree.insert(None, 13)
+    r = avlTree.insert(r, 10)
+    r = avlTree.insert(r, 15)
+    r = avlTree.insert(r, 5)
+    r = avlTree.insert(r, 11)
+    r = avlTree.insert(r, 4)
+    r = avlTree.insert(r, 8)
+    r = avlTree.insert(r, 16)
+    # avlTree.in_order(r)
+    # print("")
+    # avlTree.pre_order(r)
+    # print("")
+    # avlTree.post_order(r)
+    # print("")
+
+    avlTree.show(r)
+
+
+__test__()
